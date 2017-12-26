6f80ac429587065b5bf75c32bb37141b6b630a08
amanissery <anoop@Anoops-MacBook-Pro.local>
2017-12-26 23:00:58 +0530

    naming server and proxy changes

--- currency-conversion-service/src/main/java/com/anoop/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/anoop/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
 	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from,
@@ -34,5 +38,14 @@ public class CurrencyConversionController {
 		currencyConversionBean.setQuantity(quantity);
 		currencyConversionBean.setTotalAmount(quantity.multiply(response.getConversionMultiple()));
 		return currencyConversionBean;
+	}
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,
+			@PathVariable String to, @PathVariable BigDecimal  quantity){
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+		response.setQuantity(quantity);
+		response.setTotalAmount(quantity.multiply(response.getConversionMultiple()));
+		return response;
 	}
 }
--- currency-conversion-service/src/main/java/com/anoop/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/anoop/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,12 @@ package com.anoop.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.anoop.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
