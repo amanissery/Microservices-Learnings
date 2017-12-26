67f53fabc4636fcb0f30c2fb66c1eb9d541ace14
amanissery <anoop@Anoops-MacBook-Pro.local>
2017-12-26 10:09:43 +0530

    limits-service changes for spring cloud configuration

--- spring-cloud-config-server/src/main/java/com/anoop/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/anoop/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,8 +2,12 @@ package com.anoop.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
+@EnableConfigServer
 @SpringBootApplication
+@Configuration
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
